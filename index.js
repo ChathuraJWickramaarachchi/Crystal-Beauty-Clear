4763d82d3db6690ca7eb406f6c63a3d83b6047fd
Chathura J Wickramaarachchi <chathurajanod2003@gmail.com>
2026-01-10 13:17:50 +0530

    setup mongodb connection
    setup mongodb connection

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 import express from 'express'; //import expresss
-import bodyParser from 'body-parser'; //impport body-parser
-
+import bodyParser from 'body-parser'; //import body-parser
+import mongoose from 'mongoose'; //import mongoose
 
 //initializ the backend software to a variable
 let app = express();
+
+//mongodb connection setup 
+mongoose.connect("mongodb+srv://chathura:chathura123@cluster0.eeqbvtb.mongodb.net/?appName=Cluster0").then(
+    ()=>{
+    console.log("Successfully connected with database");
+    }
+).catch(
+    ()=>{
+        console.log("Database connection failed");
+    }
+    
+)
 
 //declaring a middleware
 app.use(bodyParser.json());
