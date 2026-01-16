0a0c56698145fda09a5717a1d4be9c342550aab2
Chathura J Wickramaarachchi <chathurajanod2003@gmail.com>
2026-01-16 17:31:43 +0530

    create new rout for items

---
+++ routes/itemRoute.js
@@ -0,0 +1,11 @@
+import express from 'express'; //import express
+import { getItem, saveItem } from '../Controller/itemController';
+
+//create new router object
+const itemRouter = express,Router();
+
+itemRouter.get("/",getItem);
+itemRouter.post("/",saveItem);
+
+//export itemRouter
+export default itemRouter;
