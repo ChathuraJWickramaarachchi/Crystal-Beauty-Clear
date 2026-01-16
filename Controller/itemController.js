d67322b243d52fdbb92daba7f8c0286ad627b709
Chathura J Wickramaarachchi <chathurajanod2003@gmail.com>
2026-01-16 17:11:01 +0530

    creat itemController.js file

---
+++ Controller/itemController.js
@@ -0,0 +1,37 @@
+import Item from "../models/item";
+
+//get requests hanler
+export function getItem(req,res){
+    Item.find().then(
+        (item)=>{
+            res.json(item);
+        }   
+     ).catch(
+        ()=>{
+            res.json({
+                "message": "Error occured while retriving item data."
+            })
+        }
+     )
+}
+
+//post requests hanler
+export function saveItem(req,res){
+    //create new saveable item object
+    const item = new Item(req,res);
+
+    //save the item data in db
+    item.save().then(
+        () =>{
+            res.json({
+                "message": "Item  data saved successfully."
+            })
+        }
+    ).catch(
+        ()=>{
+            res.json({
+                "message": "Error occured while saving ite data."
+        })
+        }
+    )
+} 
